ca06d09a026d0b6d6c85e48967a57216fa5a4e89
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-13 08:01:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,16 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max_value = 0
+  array.each do |value|
+
+    #if the value is grater than the max_value, set max_value to that value
+    if value > max_value  
+      max_value = value
+    end
+      
+  end
+  max_value
 end
 
 def find_min_value(array)
