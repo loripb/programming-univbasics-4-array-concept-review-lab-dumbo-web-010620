dd0d1de1b80da486d469f410ac7ebfde9eeea14a
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-13 08:04:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,5 +16,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min_value = nil
+  array.each do |value|
+
+    #if the value is less than the min_value, set min_value to that value
+    if value < min_value
+      min_value = value
+    end
+    min_value
+  end
 end
